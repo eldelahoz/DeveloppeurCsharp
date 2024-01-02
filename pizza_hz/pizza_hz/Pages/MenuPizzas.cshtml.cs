4d57933bdda946f751d5730b318af25473a1458d
Andres De la Hoz <andelahoz13@gmail.com>
2024-01-02 07:08:06 -0500

    Se agrega vista index y menu de pizzas

---
+++ pizza_hz/pizza_hz/Pages/MenuPizzas.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using pizza_hz.Data;
+using pizza_hz.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace pizza_hz.Pages
+{
+    public class MenuPizzasModel : PageModel
+    {
+        private readonly DataContext _dataContext;
+
+        public MenuPizzasModel(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public IList<Pizza> Pizza { get; set; }
+        public async Task OnGetAsync()
+        {
+            Pizza = await _dataContext.Pizzas.ToListAsync();
+        }
+    }
+}
